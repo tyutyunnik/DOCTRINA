ee5adb6294fbe214a569c97d4affd8d028feb531
tyutyunnik <tyutyunnik.e@gmail.com>
2023-05-05 23:44:20 +0300

    webView && menu synchronization impl

--- app/src/main/java/com/test/nmla/doctrina/presentation/fragments/WebFragment.kt
+++ app/src/main/java/com/test/nmla/doctrina/presentation/fragments/WebFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ImageButton
+import androidx.navigation.fragment.findNavController
 import com.test.nmla.doctrina.R
 import com.test.nmla.doctrina.databinding.FragmentWebBinding
 
@@ -13,6 +15,10 @@ class WebFragment : Fragment(R.layout.fragment_web) {
     private lateinit var binding: FragmentWebBinding
 
     private var link = ""
+    private lateinit var buttonIdList: ArrayList<ImageButton>
+    private var lastItemIndex = 0
+    private var element = 0
+    private var buttonState = "white"
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -20,6 +26,7 @@ class WebFragment : Fragment(R.layout.fragment_web) {
         binding = FragmentWebBinding.bind(view)
 
         link = "https://www.google.com"
+        buttonIdList = ArrayList()
 
         with(binding) {
             webView.settings.javaScriptEnabled = true
@@ -34,15 +41,62 @@ class WebFragment : Fragment(R.layout.fragment_web) {
             }
             webView.loadUrl(link)
 
+            buttonIdList.add(flagBtnMenuWeb)
 
             backBtnWeb.setOnClickListener {
+                // TODO: отследить последовательность(математика)
                 if (webView.canGoBack()) {
+                    if (buttonIdList.size != 0) {
+                        lastItemIndex = buttonIdList.size - 1
+                        if (buttonIdList[lastItemIndex - 1] == flagBtnMenuWeb) {
+                            setInactiveIcons()
+                            flagBtnMenuWeb.setImageResource(R.drawable.flag_yellow)
+                        } else if (buttonIdList[lastItemIndex - 1] == saveToBtnMenuWeb) {
+                            setInactiveIcons()
+                            saveToBtnMenuWeb.setImageResource(R.drawable.save_to_yellow)
+                        } else if (buttonIdList[lastItemIndex - 1] == chatBtnMenuWeb) {
+                            setInactiveIcons()
+                            chatBtnMenuWeb.setImageResource(R.drawable.talk_cloud_yellow)
+                        } else if (buttonIdList[lastItemIndex - 1] == starBtnMenuWeb) {
+                            setInactiveIcons()
+                            starBtnMenuWeb.setImageResource(R.drawable.star_yellow)
+                        } else if (buttonIdList[lastItemIndex - 1] == settingsBtnMenuWeb) {
+                            setInactiveIcons()
+                            settingsBtnMenuWeb.setImageResource(R.drawable.settings_yellow)
+                        }
+                    }
                     webView.goBack()
+
+//                    if (index != 0) {
+//                        element = buttonIdList[index - 1]
+//                        when (element) {
+//                            0 -> {
+//                                setInactiveIcons()
+//                                flagBtnMenuWeb.setImageResource(R.drawable.flag_yellow)
+//                            }
+//                            1 -> {
+//                                setInactiveIcons()
+//                                saveToBtnMenuWeb.setImageResource(R.drawable.save_to_yellow)
+//                            }
+//                            2 -> {
+//                                setInactiveIcons()
+//                                chatBtnMenuWeb.setImageResource(R.drawable.talk_cloud_yellow)
+//                            }
+//                            3 -> {
+//                                setInactiveIcons()
+//                                starBtnMenuWeb.setImageResource(R.drawable.star_yellow)
+//                            }
+//                            4 -> {
+//                                setInactiveIcons()
+//                                settingsBtnMenuWeb.setImageResource(R.drawable.settings_yellow)
+//                            }
+//                        }
+//                    }
                 }
             }
 
             logOutLinearLayout.setOnClickListener {
-// TODO:  
+                findNavController().navigate(R.id.action_webFragment_to_loginFragment)
             }
 
             flagBtnMenuWeb.setImageResource(R.drawable.flag_yellow)
@@ -50,47 +104,57 @@ class WebFragment : Fragment(R.layout.fragment_web) {
                 setInactiveIcons()
                 flagBtnMenuWeb.setImageResource(R.drawable.flag_yellow)
                 link = "https://www.google.com"
+//                index = 0
                 webView.loadUrl(link)
                 titleWeb.visibility = View.GONE
                 logOutLinearLayout.visibility = View.GONE
+                buttonIdList.add(flagBtnMenuWeb)
             }
 
             saveToBtnMenuWeb.setOnClickListener {
                 setInactiveIcons()
                 saveToBtnMenuWeb.setImageResource(R.drawable.save_to_yellow)
                 link = "https://github.com"
+//                index += 1
                 webView.loadUrl(link)
                 titleWeb.visibility = View.VISIBLE
                 titleWeb.text = resources.getString(R.string.choose_a_le).uppercase()
                 logOutLinearLayout.visibility = View.GONE
+                buttonIdList.add(saveToBtnMenuWeb)
             }
 
             chatBtnMenuWeb.setOnClickListener {
                 setInactiveIcons()
                 chatBtnMenuWeb.setImageResource(R.drawable.talk_cloud_yellow)
                 link = "https://stackoverflow.com/"
+//                index = 2
                 webView.loadUrl(link)
                 titleWeb.visibility = View.GONE
                 logOutLinearLayout.visibility = View.GONE
+                buttonIdList.add(chatBtnMenuWeb)
             }
 
             starBtnMenuWeb.setOnClickListener {
                 setInactiveIcons()
                 starBtnMenuWeb.setImageResource(R.drawable.star_yellow)
                 link = "https://developer.android.com/"
+//                index = 3
                 webView.loadUrl(link)
                 titleWeb.visibility = View.GONE
                 logOutLinearLayout.visibility = View.GONE
+                buttonIdList.add(starBtnMenuWeb)
             }
 
             settingsBtnMenuWeb.setOnClickListener {
                 setInactiveIcons()
                 settingsBtnMenuWeb.setImageResource(R.drawable.settings_yellow)
                 link = "https://developer.android.com/studio"
+//                index = 4
                 webView.loadUrl(link)
                 titleWeb.visibility = View.VISIBLE
                 titleWeb.text = resources.getString(R.string.your_setting).uppercase()
                 logOutLinearLayout.visibility = View.VISIBLE
+                buttonIdList.add(settingsBtnMenuWeb)
             }
         }
     }
